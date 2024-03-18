0d64222b69b64bd0a40b04d0be51a85cb668fc79
Mabedallah <mohamed.altoom@gmail.com>
2024-03-18 15:51:37 +1100

    do some changes to meet v18 react

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -12,15 +12,20 @@ const AddRestaurant = () => {
                     <input type='text' className='form-control' placeholder='Location'/>
                 </div>
                 <div className="col"> 
-                    <input type='text' className='form-control' placeholder='Price Range'/>
+                    <select type='text' className='custom-select my-1 mr-sm-2'>
+                        <option disabled>Price Range</option>
+                        <option value='1'>$</option>
+                        <option value='2'>$$</option>
+                        <option value='3'>$$$</option>
+                        <option value='4'>$$$$</option>
+                        <option value='5'>$$$$$</option>
+                    </select>
+                    <button className='btn btn-primary'>Add</button>
+                
                 </div>
-                <div className="col">
-                    <button type='submit' className='btn btn-primary'>Add</button>
                 </div>
-                </div>
-            </div>
         </form>
-        </div>
+    </div>
   )
 }
 
