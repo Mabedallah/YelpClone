812bd5ceb179f9fc140487c5ea0057510ae78174
Mabedallah <mohamed.altoom@gmail.com>
2024-03-18 17:39:34 +1100

    fix the design of adding new restaurant

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const AddRestaurant = () => {
   return (
-    <div className='mb-4'>
+    <div className='mb-4 container'>
         <form action="">
-            <div className="form-row">
-                <div className="col">
+            <div className= 'row justify-content-end'>
+                <div className='col'>
                     <input type='text' className='form-control' placeholder='Restaurant Name'/>
                 </div>
-                <div className="col"> 
+                <div className='col'> 
                     <input type='text' className='form-control' placeholder='Location'/>
                 </div>
-                <div className="col"> 
-                    <select type='text' className='custom-select my-1 mr-sm-2'>
+                <div className='col'> 
+                    <select type='text' className='form-control custom-select my-1 mr-sm-2'>
                         <option disabled>Price Range</option>
                         <option value='1'>$</option>
                         <option value='2'>$$</option>
@@ -20,8 +21,10 @@ const AddRestaurant = () => {
                         <option value='4'>$$$$</option>
                         <option value='5'>$$$$$</option>
                     </select>
-                    <button className='btn btn-primary'>Add</button>
-                
+                    
+                </div>
+                <div className='col'>
+                <button className='btn btn-primary'>Add</button>
                 </div>
                 </div>
         </form>
